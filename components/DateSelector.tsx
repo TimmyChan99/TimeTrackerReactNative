5d4654a461a62076a43c6ae6a7a8854f3d649034
Fatima Ezzahra <felemenoun@gmail.com>
2023-03-02 23:46:55 +0100

    Display the elected date and day

--- components/DateSelector.tsx
+++ components/DateSelector.tsx
@@ -2,12 +2,37 @@ import {View, Text, Button} from 'react-native';
 import React, {useState} from 'react';
 import DatePicker from 'react-native-date-picker';
 
+// Set date and day from input. This is used to display the date and day in string format
+const setDateandDay = (date: string) => {
+  const dateInput = new Date(date);
+  const dateString = dateInput.toLocaleDateString('en-GB', {
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric',
+  });
+  const day = dateInput.getDay();
+  const weekday = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+  const dayString = weekday[day];
+  return {dateString, dayString};
+};
+
 const DateSelector = () => {
   const [date, setDate] = useState(new Date());
   const [isOpened, setIsOpened] = useState(false);
+  const {dateString, dayString} = setDateandDay(date.toString());
+
   return (
     <View>
-      <Text>DateSelector</Text>
+      <Text>{dateString}</Text>
+      <Text>{dayString}</Text>
       <Button title="Select Date" onPress={() => setIsOpened(true)} />
       <DatePicker
         modal
